9a567a62aa3412d0f1102fc61607a8c12cb3b900
Ramkushal <77798529+Ramkushal@users.noreply.github.com>
2023-07-21 17:26:34 +0530

    some change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
+import Chatpage from "./pages/Chatpage";
 import Music from "./pages/Music";
 
 function App() {
-  return <div className="App">{<Music />}</div>;
+  return <div className="App">
+      <Route path="/" component={Music} exact />
+      <Route path="/chats" component={Chatpage} />
+    </div>;
 }
 
 export default App;
--- src/page/ChatPage.js
+++ src/page/ChatPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ChatPage = () => {
-  return <div></div>;
+  return <div>chat page</div>;
 };
 
 export default ChatPage;
